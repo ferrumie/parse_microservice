7c66e8a20c193d1294202f2e2e7e2b6ee9f794e0
Oluwadamilola Sonaike <56271955+damiso15@users.noreply.github.com>
2020-07-15 17:06:33 +0100

    commit

--- parse/models.py
+++ parse/models.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
 from django.db import models
-from django.core.validators import FileExtensionValidator
+from django.core.validators import FileExtensionValidator, validate_email
 
 # Create your models here.
 
@@ -9,3 +9,10 @@ class ExcelUpload(models.Model):
     document = models.FileField(upload_to='upload/', validators=[FileExtensionValidator(['xlsx'])])
     upload_at = models.DateTimeField(auto_now_add=True)
 
+
+class Email(models.Model):
+    upload = models.EmailField(max_length=50, blank=False, unique=True, validators=[validate_email])
+    upload_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.upload
